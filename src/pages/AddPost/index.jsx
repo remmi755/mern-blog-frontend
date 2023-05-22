d6ed7748f6b7a2b0f013eadaa84ea573c8d0800d
remmi755 <90457721+remmi755@users.noreply.github.com>
2023-05-22 22:12:47 +0300

    add

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -131,7 +131,7 @@ export const AddPost = () => {
           </Button>
           <img
             className={styles.image}
-            src={`https://mern-blog-new.onrender.com${imageUrl}`}
+            src={`${process.env.REACT_APP_API_URL}${imageUrl}`}
             alt="Uploaded"
           />
         </>
