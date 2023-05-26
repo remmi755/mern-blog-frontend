5c1590943d72109da5488ef2a3c9addc9045952c
remmi755 <90457721+remmi755@users.noreply.github.com>
2023-05-26 14:45:47 +0300

    add fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -64,7 +64,7 @@ export const Home = () => {
                 id={obj._id}
                 title={obj.title}
                 imageUrl={
-                  obj.imageUrl ? `https://mern-blog-new.onrender.com/${obj.imageUrl}` : ""
+                  obj.imageUrl ? `https://mern-blog-new.onrender.com${obj.imageUrl}` : ""
                 }
                 user={obj.user}
                 createdAt={obj.createdAt}
